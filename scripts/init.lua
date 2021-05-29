fe1e8c16bfa5fbf9f58ab89fd5c9be5283776e3f
PIISSI <30774618+PIISSI@users.noreply.github.com>
2021-05-29 20:42:36 +0900

    깨진하트 픽스
    ㅇㅇ

--- scripts/init.lua
+++ scripts/init.lua
@@ -8,8 +8,14 @@ end
 mod:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, mod.CurseRemove)
 
 function mod:OnUpdate(player)
-	if (Game():GetLevel():GetStage() >= 9 and player:GetBrokenHearts() < 8) then
-		player:AddBrokenHearts(8 - player:GetBrokenHearts())
+	if Game():GetLevel():GetStage() >= 9 then
+		if player:GetPlayerType() == PlayerType.PLAYER_KEEPER or player:GetPlayerType() == PlayerType.PLAYER_KEEPER_B then
+		elseif player:GetPlayerType() == PlayerType.PLAYER_THEFORGOTTEN or player:GetPlayerType() == PlayerType.PLAYER_THESOUL then
+			player:AddBrokenHearts(4 - player:GetBrokenHearts())
+			player:GetSubPlayer():AddBrokenHearts(4 - player:GetSubPlayer():GetBrokenHearts())
+		elseif player:GetBrokenHearts() < 8 then
+			player:AddBrokenHearts(8 - player:GetBrokenHearts())
+		end
 	end
 end
 
