e08155f409d6269542ee2f10ac75b3003e2dfcaa
Pdom <pdom0327@gmail.com>
2023-07-28 18:21:52 +0900

    feat: 체력 제한

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,4 +1,3 @@
-
 function Astrobirth:CurseRemove(curse) -- 입장 전 저주 제거
 	if (Game():GetLevel():GetStage() <= 3 and curse ~= 0) then
 		return 0
@@ -16,7 +15,13 @@ Astrobirth:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, Astrobirth.CurseRemove)
 
 ---@param player EntityPlayer
 function Astrobirth:OnUpdate(player)
-	if Game():GetLevel():GetStage() >= 9 then
+	local level = Game():GetLevel()
+	local stage = level:GetStage()
+
+	if
+		stage >= LevelStage.STAGE4_3 or
+			(stage == LevelStage.STAGE4_2 and level:GetStageType() == StageType.STAGETYPE_REPENTANCE)
+	 then
 		if player:GetPlayerType() == PlayerType.PLAYER_KEEPER or player:GetPlayerType() == PlayerType.PLAYER_KEEPER_B then
 		elseif player:GetPlayerType() == PlayerType.PLAYER_THEFORGOTTEN then
 			player:AddBoneHearts(-player:GetBoneHearts() + 2)
@@ -26,9 +31,23 @@ function Astrobirth:OnUpdate(player)
 			player:AddBrokenHearts(2 - player:GetBrokenHearts())
 			player:GetSubPlayer():AddBoneHearts(-player:GetSubPlayer():GetBoneHearts() + 2)
 			player:GetSubPlayer():AddBrokenHearts(4 - player:GetSubPlayer():GetBrokenHearts())
-		elseif player:GetBrokenHearts() < 8 then
-			player:AddMaxHearts(-player:GetEffectiveMaxHearts() + 2, true)
-			player:AddBrokenHearts(8 - player:GetBrokenHearts())
+		else
+			if player:GetEffectiveMaxHearts() > 2 then
+				if player:GetMaxHearts() >= 2 then
+					player:AddMaxHearts(2 - player:GetMaxHearts(), true)
+					player:AddBoneHearts(-player:GetBoneHearts())
+				else
+					player:AddBoneHearts(1 - player:GetBoneHearts())
+				end
+			end
+
+			if player:GetSoulHearts() > 6 then
+				player:AddSoulHearts(6 - player:GetSoulHearts())
+			end
+
+			if player:GetBrokenHearts() < 8 then
+				player:AddBrokenHearts(8 - player:GetBrokenHearts())
+			end
 		end
 	end
 end
