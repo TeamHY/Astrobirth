a7e698abcffd95fe9fefd7105be8c2b7a02f2bf6
Pdom <pdom0327@gmail.com>
2023-08-27 21:57:15 +0900

    feat: absolute zero

---
+++ astro/trinkets/absolute-zero.lua
@@ -0,0 +1,29 @@
+Astro.Trinket.ABSOLUTE_ZERO = Isaac.GetTrinketIdByName("Absolute Zero")
+
+if EID then
+    EID:addTrinket(Astro.Trinket.ABSOLUTE_ZERO, "#{{Freezing}} 10%의 확률로 적 처치시 적이 얼어붙는 눈물이 나갑니다.#{{Freezing}} 얼어붙은 적은 접촉 시 직선으로 날아가 10방향으로 고드름 눈물을 발사합니다.#!!! {{LuckSmall}}행운 수치 비례: 행운 45 이상일 때 100% 확률", "절대 영도")
+
+    Astro.Utill:addGoldenTrinketDescription(Astro.Trinket.ABSOLUTE_ZERO, "", 10)
+end
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_FIRE_TEAR,
+    ---@param tear EntityTear
+    function(_, tear)
+        local player = Astro.Utill:GetPlayerFromEntity(tear)
+
+        if player ~= nil then
+            if
+                player:HasTrinket(Astro.Trinket.ABSOLUTE_ZERO) and
+                    not player:HasCollectible(CollectibleType.COLLECTIBLE_URANUS)
+             then
+                local rng = player:GetTrinketRNG(Astro.Trinket.ABSOLUTE_ZERO)
+
+                if rng:RandomFloat() < 0.1 * player:GetTrinketMultiplier(Astro.Trinket.ABSOLUTE_ZERO) + player.Luck / 50 then
+                    tear.TearFlags = tear.TearFlags | TearFlags.TEAR_ICE
+                    tear:ChangeVariant(TearVariant.ICE)
+                end
+            end
+        end
+    end
+)
