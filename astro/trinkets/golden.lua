54102c92fa47fa0aba4c0a1fa80f28234b602733
Pdom <pdom0327@gmail.com>
2023-09-09 17:16:58 +0900

    feat: 황금 장신구
    -#TEMPORARY_TATTOO : 미니맵에 전투방 위치를 표시해주고, 전투방을 조건없이 들어갈 수 있음
    -#MATCH_STICK : 스테이지 진입 시 기가 폭탄 소환(5.40.7)

--- astro/trinkets/golden.lua
+++ astro/trinkets/golden.lua
@@ -1,42 +1,47 @@
--- TrinketType.TRINKET_UMBILICAL_CORD : 해당 장신구 증발되고, 리틀 스티븐(CollectibleType.COLLECTIBLE_LITTLE_STEVEN) 지급 []
--- TrinketType.TRINKET_MISSING_PAGE : 해당 장신구 증발되고, 네크로노미콘(CollectibleType.COLLECTIBLE_NECRONOMICON) 소환 [] //
--- TrinketType.TRINKET_FADED_POLAROID : 해당 장신구 증발되고, 네거티브, 폴라로이드 2개 등장 후 한개 획득 []
--- TrinketType.TRINKET_LOUSE : 해당 장신구 증발되고, 인페2(CollectibleType.COLLECTIBLE_INFESTATION_2) 소환 [] //
--- TrinketType.TRINKET_BROKEN_SYRINGE : 해당 장신구 증발되면서 아이작에 존재하는 주사기 2개 소환되고 1개 선택 [] //
-
--- TrinketType.TRINKET_SILVER_DOLLAR : 황금으로 되는 즉시 캐릭터에게 흡수 []
--- TrinketType.TRINKET_BLOODY_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수 []
--- TrinketType.TRINKET_HOLY_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수 []
--- TrinketType.TRINKET_WICKED_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수 []
--- TrinketType.TRINKET_NUMBER_MAGNET : 황금으로 되는 즉시 캐릭터에게 흡수 []
-
--- TrinketType.TRINKET_PURPLE_HEART : 배수 1.2배 증가  []
-
--- TrinketType.TRINKET_PINKY_EYE : 공격 시 100% 확률로 독눈물 []
--- TrinketType.TRINKET_PUSH_PIN : 공격 시 100% 확률로 관통 눈물 []
-
--- TrinketType.TRINKET_SUPER_BALL : 공격 시 25% 확률로 시맨트 적용 (럭1당 1%p 증가) []
--- TrinketType.TRINKET_BRAIN_WORM : 공격 시 50% 확률로 유도 적용 (럭1당 1%p 증가) []
-
--- TrinketType.TRINKET_BLACK_LIPSTICK : 스테이지 진입 시 블랙 하트 1개 소환 [] //
--- TrinketType.TRINKET_RUSTED_KEY : 스테이지 진입 시 마다 금키 소환 []
--- TrinketType.TRINKET_SAFETY_CAP : 스테이지 진입 시 마다 걸프 알약 소환 []
--- TrinketType.TRINKET_ACE_SPADES : 스테이지 진입 시 마다 랜덤 카드 소환 []
-
--- TrinketType.TRINKET_BLOODY_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 빨간 하트 반칸 드랍 (럭1당 1%p 증가) []
--- TrinketType.TRINKET_BURNT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 폭탄 소환 (럭1당 1%p 증가) []
--- TrinketType.TRINKET_FLAT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 열쇠 소환 (럭1당 1%p 증가) []
--- TrinketType.TRINKET_COUNTERFEIT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 동전 1개 소환 (럭1당 1%p 증가) []
--- TrinketType.TRINKET_ROTTEN_PENNY : 동전 획득 시 종류 상관 없이 100% 확률로 자폭 파리 하나 소환 []
--- TrinketType.TRINKET_BLESSED_PENNY : 동전 획득 시 종류 상관 없이 12.5% 확률로 소울 하트 반칸 소환 (럭1당 1%p 증가) []
-
--- TrinketType.TRINKET_BAT_WING : 소지중인 상태일경우 공중 적용 []
+-- TrinketType.TRINKET_UMBILICAL_CORD : 해당 장신구 증발되고, 리틀 스티븐(CollectibleType.COLLECTIBLE_LITTLE_STEVEN) 지급
+-- TrinketType.TRINKET_MISSING_PAGE : 해당 장신구 증발되고, 네크로노미콘(CollectibleType.COLLECTIBLE_NECRONOMICON) 소환
+-- TrinketType.TRINKET_FADED_POLAROID : 해당 장신구 증발되고, 네거티브, 폴라로이드 2개 등장 후 한개 획득
+-- TrinketType.TRINKET_LOUSE : 해당 장신구 증발되고, 인페2(CollectibleType.COLLECTIBLE_INFESTATION_2) 소환
+-- TrinketType.TRINKET_BROKEN_SYRINGE : 해당 장신구 증발되면서 아이작에 존재하는 주사기 2개 소환되고 1개 선택
 -- TrinketType.TRINKET_RED_PATCH : 해당 장신구 증발되고, 블러디 러스트 소환
+
+-- TrinketType.TRINKET_SILVER_DOLLAR : 황금으로 되는 즉시 캐릭터에게 흡수
+-- TrinketType.TRINKET_BLOODY_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수
+-- TrinketType.TRINKET_HOLY_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수
+-- TrinketType.TRINKET_WICKED_CROWN : 황금으로 되는 즉시 캐릭터에게 흡수
+-- TrinketType.TRINKET_NUMBER_MAGNET : 황금으로 되는 즉시 캐릭터에게 흡수
+
+-- TrinketType.TRINKET_PURPLE_HEART : 배수 1.2배 증가
+
+-- TrinketType.TRINKET_PINKY_EYE : 공격 시 100% 확률로 독눈물
+-- TrinketType.TRINKET_PUSH_PIN : 공격 시 100% 확률로 관통 눈물
+
+-- TrinketType.TRINKET_SUPER_BALL : 공격 시 25% 확률로 시맨트 적용 (럭1당 1%p 증가)
+-- TrinketType.TRINKET_BRAIN_WORM : 공격 시 50% 확률로 유도 적용 (럭1당 1%p 증가)
+
+-- TrinketType.TRINKET_BLACK_LIPSTICK : 스테이지 진입 시 블랙 하트 1개 소환
+-- TrinketType.TRINKET_RUSTED_KEY : 스테이지 진입 시 마다 금키 소환
+-- TrinketType.TRINKET_SAFETY_CAP : 스테이지 진입 시 마다 걸프 알약 소환
+-- TrinketType.TRINKET_ACE_SPADES : 스테이지 진입 시 마다 랜덤 카드 소환
+-- TrinketType.TRINKET_MATCH_STICK : 스테이지 진입 시 기가 폭탄 소환(5.40.7)
+
+-- TrinketType.TRINKET_BLOODY_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 빨간 하트 반칸 드랍 (럭1당 1%p 증가)
+-- TrinketType.TRINKET_BURNT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 폭탄 소환 (럭1당 1%p 증가)
+-- TrinketType.TRINKET_FLAT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 열쇠 소환 (럭1당 1%p 증가)
+-- TrinketType.TRINKET_COUNTERFEIT_PENNY : 동전 획득 시 종류 상관 없이 25% 확률로 랜덤 동전 1개 소환 (럭1당 1%p 증가)
+-- TrinketType.TRINKET_ROTTEN_PENNY : 동전 획득 시 종류 상관 없이 100% 확률로 자폭 파리 하나 소환
+-- TrinketType.TRINKET_BLESSED_PENNY : 동전 획득 시 종류 상관 없이 12.5% 확률로 소울 하트 반칸 소환 (럭1당 1%p 증가)
+
+-- TrinketType.TRINKET_BAT_WING : 소지중인 상태일경우 공중 적용
+
+-- TrinketType.TRINKET_TEMPORARY_TATTOO : 미니맵에 전투방 위치를 표시해주고, 전투방을 조건없이 들어갈 수 있음
 
 local isc = require("astro.lib.isaacscript-common")
 
 local GRID_SIZE = 40
 local GOLDEN_TRINKET_OFFSET = 32768
+
+local game = Game()
 
 if EID then
     Astro.Utill:addGoldenTrinketDescription(
@@ -54,13 +59,17 @@ if EID then
     Astro.Utill:addGoldenTrinketDescription(
         TrinketType.TRINKET_LOUSE,
         {
-            "효과 2배 #!!! {{ColorGold}}획득 시 사라지고 {{Collectible234}} 소환합니다.",
-            "효과 3배 #!!! {{ColorGold}}해당 장신구는 사라지고 {{Collectible234}} 소환합니다."
+            "효과 2배#!!! {{ColorGold}}획득 시 사라지고 {{Collectible234}} 소환합니다.",
+            "효과 3배#!!! {{ColorGold}}획득 시 사라지고 {{Collectible234}} 소환합니다."
         }
     )
     Astro.Utill:addGoldenTrinketDescription(
         TrinketType.TRINKET_BROKEN_SYRINGE,
         {"!!! 획득 시 사라지고 랜덤한 주사기 아이템 2개를 소환합니다. 하나를 선택하면 나머지는 사라집니다."}
+    )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_RED_PATCH,
+        {"!!! 획득 시 사라지고 {{Collectible157}}BloodyLust을 소환합니다."}
     )
 
     Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_SILVER_DOLLAR, {"!!! 획득 시 바로 흡수됩니다."})
@@ -83,24 +92,46 @@ if EID then
         "눈물을 발사할 때 25%의 확률로 유도 효과가 생깁니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 50 이상일 때 100% 확률"
     )
 
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BLACK_LIPSTICK, "스테이지를 넘어갈 때마다 {{BlackHeart}}블랙하트를 하나 드랍합니다.")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_RUSTED_KEY, "스테이지를 넘어갈 때마다 {{GoldenKey}}황금열쇠를 하나 드랍합니다.")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_SAFETY_CAP, "스테이지를 넘어갈 때마다 {{Pill1}}Gulp! 알약을 하나 드랍합니다.")
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_BLACK_LIPSTICK,
+        "스테이지를 넘어갈 때마다 {{BlackHeart}}블랙하트를 하나 드랍합니다."
+    )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_RUSTED_KEY,
+        "스테이지를 넘어갈 때마다 {{GoldenKey}}황금열쇠를 하나 드랍합니다."
+    )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_SAFETY_CAP,
+        "스테이지를 넘어갈 때마다 {{Pill1}}Gulp! 알약을 하나 드랍합니다."
+    )
     Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_ACE_SPADES, "스테이지를 넘어갈 때마다 {{Card}}카드를 하나 생성합니다.")
-
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BLOODY_PENNY, "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BURNT_PENNY, "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_FLAT_PENNY, "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_COUNTERFEIT_PENNY, "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_ROTTEN_PENNY, "효과가 1번 더 발동합니다.")
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BLESSED_PENNY, "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 88 이상일 때 100% 확률")
-
-    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BAT_WING, "!!! 비행 능력을 얻습니다.")
+    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_MATCH_STICK, "스테이지를 넘어갈 때마다 {{Bomb}}기가 폭탄을 하나 드랍합니다.")
 
     Astro.Utill:addGoldenTrinketDescription(
-        TrinketType.TRINKET_RED_PATCH,
-        {"!!! 획득 시 사라지고 {{Collectible157}}BloodyLust을 소환합니다."}
+        TrinketType.TRINKET_BLOODY_PENNY,
+        "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률"
     )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_BURNT_PENNY,
+        "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률"
+    )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_FLAT_PENNY,
+        "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률"
+    )
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_COUNTERFEIT_PENNY,
+        "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 75 이상일 때 100% 확률"
+    )
+    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_ROTTEN_PENNY, "효과가 1번 더 발동합니다.")
+    Astro.Utill:addGoldenTrinketDescription(
+        TrinketType.TRINKET_BLESSED_PENNY,
+        "효과가 1번 더 발동합니다. 동전 가치를 반영하지 않습니다.#!!! {{ColorGold}}{{LuckSmall}}행운 수치 비례: 행운 88 이상일 때 100% 확률"
+    )
+
+    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_BAT_WING, "비행 능력을 얻습니다.")
+
+    Astro.Utill:addGoldenTrinketDescription(TrinketType.TRINKET_TEMPORARY_TATTOO, "{{ChallengeRoom}} 소지 중일 때 도전방/보스도전방에 항상 입장할 수 있습니다.#{{ColorGold}}맵에 {{ChallengeRoom}}도전방/보스도전방의 위치가 표시됩니다.")
 end
 
 ---@param value integer
@@ -122,7 +153,7 @@ local function RunEffect(player, type)
         player:AddCollectible(CollectibleType.COLLECTIBLE_LITTLE_STEVEN)
         return true
     elseif CheckTrinket(type, TrinketType.TRINKET_MISSING_PAGE) then
-        local currentRoom = Game():GetLevel():GetCurrentRoom()
+        local currentRoom = game:GetLevel():GetCurrentRoom()
         Isaac.Spawn(
             EntityType.ENTITY_PICKUP,
             PickupVariant.PICKUP_COLLECTIBLE,
@@ -133,7 +164,7 @@ local function RunEffect(player, type)
         )
         return true
     elseif CheckTrinket(type, TrinketType.TRINKET_RED_PATCH) then
-        local currentRoom = Game():GetLevel():GetCurrentRoom()
+        local currentRoom = game:GetLevel():GetCurrentRoom()
         Isaac.Spawn(
             EntityType.ENTITY_PICKUP,
             PickupVariant.PICKUP_COLLECTIBLE,
@@ -144,7 +175,7 @@ local function RunEffect(player, type)
         )
         return true
     elseif CheckTrinket(type, TrinketType.TRINKET_FADED_POLAROID) then
-        local currentRoom = Game():GetLevel():GetCurrentRoom()
+        local currentRoom = game:GetLevel():GetCurrentRoom()
         Isaac.Spawn(
                 EntityType.ENTITY_PICKUP,
                 PickupVariant.PICKUP_COLLECTIBLE,
@@ -171,7 +202,7 @@ local function RunEffect(player, type)
             ):ToPickup().OptionsPickupIndex = TrinketType.TRINKET_FADED_POLAROID
         return true
     elseif CheckTrinket(type, TrinketType.TRINKET_LOUSE) then
-        local currentRoom = Game():GetLevel():GetCurrentRoom()
+        local currentRoom = game:GetLevel():GetCurrentRoom()
         Isaac.Spawn(
             EntityType.ENTITY_PICKUP,
             PickupVariant.PICKUP_COLLECTIBLE,
@@ -182,7 +213,7 @@ local function RunEffect(player, type)
         )
         return true
     elseif CheckTrinket(type, TrinketType.TRINKET_BROKEN_SYRINGE) then
-        local currentRoom = Game():GetLevel():GetCurrentRoom()
+        local currentRoom = game:GetLevel():GetCurrentRoom()
         local rng = player:GetTrinketRNG(TrinketType.TRINKET_BROKEN_SYRINGE)
 
         local syringes = {
@@ -280,6 +311,27 @@ Astro:AddCallback(
          then
             player:TryRemoveTrinket(trinket1)
         end
+
+        if player:GetTrinketMultiplier(TrinketType.TRINKET_TEMPORARY_TATTOO) > 1 then
+            local level = game:GetLevel()
+            local idx = level:QueryRoomTypeIndex(RoomType.ROOM_CHALLENGE, false, RNG())
+            local room = level:GetRoomByIdx(idx)
+    
+            if room.Data.Type == RoomType.ROOM_CHALLENGE then
+                room.DisplayFlags = room.DisplayFlags | 1 << 2
+                level:UpdateVisibility()
+            end
+            
+            local currentRoom = Game():GetLevel():GetCurrentRoom()
+
+            for _, value in pairs(isc:getDoors()) do
+                local door = value ---@type GridEntityDoor
+
+                if currentRoom:IsClear() and door:IsOpen() == false and door.TargetRoomType == RoomType.ROOM_CHALLENGE then
+                    door:TryUnlock(Isaac.GetPlayer(), true)
+                end
+            end
+        end
     end
 )
 
@@ -353,9 +405,9 @@ Astro:AddCallback(
 Astro:AddCallback(
     ModCallbacks.MC_POST_NEW_LEVEL,
     function()
-        for i = 1, Game():GetNumPlayers() do
+        for i = 1, game:GetNumPlayers() do
             local player = Isaac.GetPlayer(i - 1)
-            local currentRoom = Game():GetLevel():GetCurrentRoom()
+            local currentRoom = game:GetLevel():GetCurrentRoom()
 
             if player:GetTrinketMultiplier(TrinketType.TRINKET_BLACK_LIPSTICK) > 1 then
                 Isaac.Spawn(
@@ -380,7 +432,7 @@ Astro:AddCallback(
             end
 
             if player:GetTrinketMultiplier(TrinketType.TRINKET_SAFETY_CAP) > 1 then
-                local itemPool = Game():GetItemPool()
+                local itemPool = game:GetItemPool()
                 local pillColor = itemPool:ForceAddPillEffect(PillEffect.PILLEFFECT_GULP)
 
                 Isaac.Spawn(
@@ -403,6 +455,17 @@ Astro:AddCallback(
                     nil
                 )
             end
+
+            if player:GetTrinketMultiplier(TrinketType.TRINKET_MATCH_STICK) > 1 then
+                Isaac.Spawn(
+                    EntityType.ENTITY_PICKUP,
+                    PickupVariant.PICKUP_BOMB,
+                    BombSubType.BOMB_GIGA,
+                    currentRoom:FindFreePickupSpawnPosition(player.Position, GRID_SIZE, true),
+                    Vector.Zero,
+                    nil
+                )
+            end
         end
     end
 )
@@ -413,7 +476,7 @@ Astro:AddCallbackCustom(
     ---@param player EntityPlayer
     function(_, pickup, player)
         if pickup.Variant == PickupVariant.PICKUP_COIN then
-            local currentRoom = Game():GetLevel():GetCurrentRoom()
+            local currentRoom = game:GetLevel():GetCurrentRoom()
 
             if player:GetTrinketMultiplier(TrinketType.TRINKET_BLOODY_PENNY) > 1 then
                 local rng = player:GetTrinketRNG(TrinketType.TRINKET_BLOODY_PENNY)
