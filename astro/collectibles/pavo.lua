ee5bc146e903bbf6cd94def6f7080152d7f768ea
Pdom <pdom0327@gmail.com>
2023-09-12 01:19:28 +0900

    fix: pavo
    메가사탄 20%로 가능하더라구요

--- astro/collectibles/pavo.lua
+++ astro/collectibles/pavo.lua
@@ -1,7 +1,7 @@
 Astro.Collectible.PAVO = Isaac.GetItemIdByName("Pavo")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.PAVO, "모든 몬스터들이 체력 25% 감소됩니다.", "공작자리")
+    EID:addCollectible(Astro.Collectible.PAVO, "Mega Satan 1 페이즈을 제외한 모든 몬스터들의 체력이 25% 감소됩니다.#Mega Satan 1 페이즈의 체력이 20% 감소됩니다.", "공작자리")
 end
 
 Astro:AddCallback(
@@ -9,13 +9,17 @@ Astro:AddCallback(
     ---@param entity Entity
     function(_, entity)
         local isRun = false
-        
+
         for i = 1, Game():GetNumPlayers() do
             local player = Isaac.GetPlayer(i - 1)
 
             if player:HasCollectible(Astro.Collectible.PAVO) and not isRun then
                 if entity:IsVulnerableEnemy() and entity.Type ~= EntityType.ENTITY_FIREPLACE then
-                    entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.25
+                    if entity.Type == EntityType.ENTITY_MEGA_SATAN then
+                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.2
+                    else
+                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.25
+                    end
                 end
 
                 isRun = true
