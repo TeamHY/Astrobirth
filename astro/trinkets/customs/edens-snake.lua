879bec4fd39e9e58145002012b5cfd6b70737e78
Pdom <pdom0327@gmail.com>
2023-10-10 16:09:19 +0900

    feat: 에덴의 뱀 변경
    도그마에서 바로 획득하도록 변경

--- astro/trinkets/customs/edens-snake.lua
+++ astro/trinkets/customs/edens-snake.lua
@@ -3,7 +3,7 @@ Astro.Trinket.EDENS_SNAKE = Isaac.GetTrinketIdByName("Eden's Snake")
 if EID then
     EID:addTrinket(
         Astro.Trinket.EDENS_SNAKE,
-        "소지한 상태에서 Mega Satan, Delirium, Mother, Dogma 처치 시 Eden's Blessing 1개를 소환합니다.#!!! 효과가 발동한 뒤 사라집니다.",
+        "소지한 상태에서 Mega Satan, Delirium, Mother 처치 시 Eden's Blessing 1개를 소환합니다.#Dogma 처치 시 Eden's Blessing 1개를 획득합니다.#!!! 효과가 발동한 뒤 사라집니다.",
         "에덴의 뱀"
     )
 
@@ -24,27 +24,20 @@ Astro:AddCallback(
             if
                 player:HasTrinket(Astro.Trinket.EDENS_SNAKE) and
                     (npc.Type == EntityType.ENTITY_MEGA_SATAN_2 or npc.Type == EntityType.ENTITY_DELIRIUM or
-                        npc.Type == EntityType.ENTITY_MOTHER or
-                        npc.Type == EntityType.ENTITY_DOGMA)
+                        npc.Type == EntityType.ENTITY_MOTHER)
              then
-                Isaac.Spawn(
-                    EntityType.ENTITY_PICKUP,
-                    PickupVariant.PICKUP_COLLECTIBLE,
-                    CollectibleType.COLLECTIBLE_EDENS_BLESSING,
-                    currentRoom:FindFreePickupSpawnPosition(player.Position + Vector(0, -GRID_SIZE)),
-                    Vector.Zero,
-                    nil
-                )
+                Astro:SpawnCollectible(CollectibleType.COLLECTIBLE_EDENS_BLESSING, player.Position)
 
                 if player:GetTrinketMultiplier(Astro.Trinket.EDENS_SNAKE) > 1 then
-                    Isaac.Spawn(
-                        EntityType.ENTITY_PICKUP,
-                        PickupVariant.PICKUP_COLLECTIBLE,
-                        CollectibleType.COLLECTIBLE_EDENS_BLESSING,
-                        currentRoom:FindFreePickupSpawnPosition(player.Position + Vector(GRID_SIZE, -GRID_SIZE)),
-                        Vector.Zero,
-                        nil
-                    )
+                    Astro:SpawnCollectible(CollectibleType.COLLECTIBLE_EDENS_BLESSING, player.Position)
+                end
+
+                player:TryRemoveTrinket(Astro.Trinket.EDENS_SNAKE)
+            elseif player:HasTrinket(Astro.Trinket.EDENS_SNAKE) and npc.Type == EntityType.ENTITY_DOGMA then
+                player:AddCollectible(CollectibleType.COLLECTIBLE_EDENS_BLESSING)
+
+                if player:GetTrinketMultiplier(Astro.Trinket.EDENS_SNAKE) > 1 then
+                    player:AddCollectible(CollectibleType.COLLECTIBLE_EDENS_BLESSING)
                 end
 
                 player:TryRemoveTrinket(Astro.Trinket.EDENS_SNAKE)
