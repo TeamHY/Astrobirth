32274586a8712883ea67822a830799957eda1a41
Pdom <pdom0327@gmail.com>
2023-10-25 09:54:09 +0900

    feat: blood of hatred #21

--- astro/collectibles/customs/blood-of-hatred.lua
+++ astro/collectibles/customs/blood-of-hatred.lua
@@ -1,36 +1,51 @@
--- local isc = require("astro.lib.isaacscript-common")
+Astro.Collectible.BLOOD_OF_HATRED = Isaac.GetItemIdByName("Blood Of Hatred")
 
--- Astro.Collectible.BLOOD_OF_HATRED = Isaac.GetItemIdByName("Blood Of Hatred")
+if EID then
+    EID:addCollectible(Astro.Collectible.BLOOD_OF_HATRED, "방 입장 시 몬스터들이 출혈 상태가 됩니다 (기본 25%, 럭1당 2.5%p 증가)", "증오의 피")
+end
 
--- if EID then
---     EID:addCollectible(Astro.Collectible.BLOOD_OF_HATRED, "방 입장 시 모든 몬스터가 5초간 {{Freezing}}빙결 상태가 됩니다 ({{Trinket188}} 만럭 효과와 동일)#다음 게임 시작 시 {{Collectible302}}Leo를 가지고 시작합니다.", "초 사자자리")
--- end
+Astro:AddCallback(
+    ModCallbacks.MC_POST_NEW_ROOM,
+    function()
+        for i = 1, Game():GetNumPlayers() do
+            local player = Isaac.GetPlayer(i - 1)
 
--- Astro:AddCallback(
---     ModCallbacks.MC_POST_NEW_ROOM,
---     function()
---         for i = 1, Game():GetNumPlayers() do
---             local player = Isaac.GetPlayer(i - 1)
+            if player:HasCollectible(Astro.Collectible.BLOOD_OF_HATRED) then
+                local entities = Isaac.GetRoomEntities()
 
---             if player:HasCollectible(Astro.Collectible.BLOOD_OF_HATRED) then
---                 local entities = Isaac.GetRoomEntities()
+                for _, entity in ipairs(entities) do
+                    if entity:IsVulnerableEnemy() and entity.Type ~= EntityType.ENTITY_FIREPLACE then
+                        local rng = player:GetCollectibleRNG(Astro.Collectible.BLOOD_OF_HATRED)
 
---                 for _, entity in ipairs(entities) do
---                     if entity:IsVulnerableEnemy() and entity.Type ~= EntityType.ENTITY_FIREPLACE then
---                         local tear = Isaac.Spawn(
---                             EntityType.ENTITY_TEAR,
---                             TearVariant.BLUE,
---                             0,
---                             entity.Position,
---                             Vector.Zero,
---                             player
---                         ):ToTear()
---                         tear.TearFlags = tear.TearFlags | TearFlags.TEAR_BACKSTAB | TearFlags.TEAR_LUDOVICO
---                         tear.CollisionDamage = 0
---                     end
---                 end
---                 break
---             end
---         end
---     end
--- )
+                        if rng:RandomFloat() < 0.25 + player.Luck / 40 then
+                            entity:GetData().BloodOfHatred = {
+                                DurationTime = entity.FrameCount + 150 -- 5초
+                            }
+    
+                            entity:AddEntityFlags(EntityFlag.FLAG_BLEED_OUT)
+                        end
+                    end
+                end
+
+                -- 여러 플레이어가 가지고 있으면 각각 발동한다
+            end
+        end
+    end
+)
+
+-- TODO: Aquarius EX와 로직 통합하기
+Astro:AddCallback(
+    ModCallbacks.MC_POST_UPDATE,
+    function()
+        local entities = Isaac.GetRoomEntities()
+
+        for _, entity in ipairs(entities) do
+            local data = entity:GetData().BloodOfHatred
+
+            if data ~= nil and data.DurationTime <= entity.FrameCount then
+                entity:ClearEntityFlags(EntityFlag.FLAG_BLEED_OUT)
+                entity:GetData().BloodOfHatred = nil
+            end
+        end
+    end
+)
