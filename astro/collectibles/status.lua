24a7f92ed1220f1f9a23135d50af6e6fbb40375a
Pdom <pdom0327@gmail.com>
2023-11-05 03:30:42 +0900

    feat: 루도비코 #31

--- astro/collectibles/status.lua
+++ astro/collectibles/status.lua
@@ -9,14 +9,14 @@ if EID then
         function(descObj)
             if
                 descObj.ObjSubType == CollectibleType.COLLECTIBLE_LITTLE_HORN or
-                    descObj.ObjSubType == CollectibleType.COLLECTIBLE_EUTHANASIA or
-                    descObj.ObjSubType == CollectibleType.COLLECTIBLE_LOST_CONTACT
-             then
+                descObj.ObjSubType == CollectibleType.COLLECTIBLE_EUTHANASIA or
+                descObj.ObjSubType == CollectibleType.COLLECTIBLE_LOST_CONTACT
+            then
                 EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.75")
             elseif
                 descObj.ObjSubType == CollectibleType.COLLECTIBLE_GHOST_PEPPER or
-                    descObj.ObjSubType == CollectibleType.COLLECTIBLE_BIRDS_EYE
-             then
+                descObj.ObjSubType == CollectibleType.COLLECTIBLE_BIRDS_EYE
+            then
                 EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.5#↓ {{LuckSmall}}행운 -5")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_C_SECTION then
                 EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.75")
@@ -34,6 +34,8 @@ if EID then
                 EID:appendToDescription(descObj, "#↓ {{LuckSmall}}행운 -1")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_MERCURIUS then
                 EID:appendToDescription(descObj, "#↓ 최대 이동 속도가 1.6으로 제한이 됩니다")
+            elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_LUDOVICO_TECHNIQUE then
+                EID:appendToDescription(descObj, "#↑ {{ShotspeedSmall}}탄속 +0.5")
             end
 
             return descObj
@@ -106,6 +108,10 @@ Astro:AddCallback(
                 if player.MoveSpeed > 1.6 then
                     player.MoveSpeed = 1.6
                 end
+            end
+        elseif cacheFlag == CacheFlag.CACHE_SHOTSPEED then
+            if player:HasCollectible(CollectibleType.COLLECTIBLE_LUDOVICO_TECHNIQUE) then
+                player.ShotSpeed = player.ShotSpeed + 0.5
             end
         end
     end
