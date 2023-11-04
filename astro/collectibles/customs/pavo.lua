bae5a1428d207590b948c7a46e980b5ea9d47371
iwt2hw <iwt2hw@naver.com>
2023-11-04 14:51:39 +0900

    파보 수치 조정
    파보 수치 조정

--- astro/collectibles/customs/pavo.lua
+++ astro/collectibles/customs/pavo.lua
@@ -1,7 +1,7 @@
 Astro.Collectible.PAVO = Isaac.GetItemIdByName("Pavo")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.PAVO, "Mega Satan 1 페이즈을 제외한 모든 몬스터들의 체력이 18% 감소됩니다.#Mega Satan 1 페이즈의 체력이 20% 감소됩니다.", "공작자리")
+    EID:addCollectible(Astro.Collectible.PAVO, "Mega Satan 1 페이즈을 제외한 모든 몬스터들의 체력이 15% 감소됩니다.#Mega Satan 1 페이즈의 체력이 10% 감소됩니다.", "공작자리")
 end
 
 Astro:AddCallback(
@@ -14,9 +14,9 @@ Astro:AddCallback(
             if player:HasCollectible(Astro.Collectible.PAVO) then
                 if entity:IsVulnerableEnemy() and entity.Type ~= EntityType.ENTITY_FIREPLACE then
                     if entity.Type == EntityType.ENTITY_MEGA_SATAN then
-                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.2
+                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.1
                     else
-                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.18
+                        entity.HitPoints = entity.HitPoints - entity.MaxHitPoints * 0.15
                     end
                 end
 
