f8b39d2c0d7432caab00e94314caf2b8f9691b18
Pdom <pdom0327@gmail.com>
2023-12-28 05:29:00 +0900

    feat: epic effect 등장 시로 수정

--- astro/epic.lua
+++ astro/epic.lua
@@ -8,6 +8,9 @@ local alpha = 1
 -- 볼륨 설정
 local volume = 0.7
 
+-- 지속 프레임
+local duration = 10 * 30 -- 10초
+
 local epics = {
     [RoomType.ROOM_TREASURE] = {
         CollectibleType.COLLECTIBLE_D6,
@@ -17,43 +20,57 @@ local epics = {
     },
 }
 
-local sprite = Sprite()
-sprite:Load("gfx/effects/epic.anm2", true)
-sprite.Color = Color(1, 1, 1, alpha, 0, 0, 0)
-
----@type EntityPlayer?
-local targetPlayer = nil
+---@type {pickup: EntityPickup, sprite: Sprite, endFrame: integer}[]
+local targets = {}
 
 Astro:AddCallback(
     ModCallbacks.MC_POST_UPDATE,
     function(_)
-        sprite:Update()
+        for i, target in ipairs(targets) do
+            target.sprite:Update()
+        end
     end
 )
 
 Astro:AddCallback(
     ModCallbacks.MC_POST_RENDER,
     function(_)
-        if targetPlayer then
-            sprite:Render(Isaac.WorldToRenderPosition(targetPlayer.Position + Vector(0, -56)), Vector(0, 0), Vector(0, 0))
+        local removeList = {}
+
+        for i, target in ipairs(targets) do
+            if target.pickup:Exists() and target.pickup.SubType ~= 0 and Game():GetFrameCount() < target.endFrame then
+                target.sprite:Render(Isaac.WorldToRenderPosition(target.pickup.Position + Vector(0, -40)), Vector(0, 0), Vector(0, 0))
+            else
+                table.insert(removeList, i)
+            end
+        end
+
+        for i, _ in ipairs(removeList) do
+            table.remove(targets, i)
         end
     end
 )
 
-Astro:AddCallbackCustom(
-    isc.ModCallbackCustom.PRE_ITEM_PICKUP,
-    ---@param player EntityPlayer
-    ---@param pickingUpItem { itemType: ItemType, subType: CollectibleType | TrinketType }
-    function(_, player, pickingUpItem)
-        local roomType = Game():GetLevel():GetCurrentRoom():GetType()
+Astro:AddCallback(
+    ModCallbacks.MC_POST_PICKUP_INIT,
+    ---@param pickup EntityPickup
+    function(_, pickup)
+        if pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+            local roomType = Game():GetLevel():GetCurrentRoom():GetType()
 
-        if epics[roomType] then
-            for _, epic in ipairs(epics[roomType]) do
-                if pickingUpItem.itemType ~= ItemType.ITEM_TRINKET and pickingUpItem.subType == epic then
-                    targetPlayer = player
-                    sprite:Play("Play", true)
-                    SFXManager():Play(soundId, volume)
-                    break
+            if epics[roomType] then
+                for _, epic in ipairs(epics[roomType]) do
+                    if pickup.SubType == epic then
+                        local sprite = Sprite()
+                        sprite:Load("gfx/effects/epic.anm2", true)
+                        sprite.Color = Color(1, 1, 1, alpha, 0, 0, 0)
+                        sprite:Play("Play", true)
+
+                        table.insert(targets, { pickup = pickup, sprite = sprite, endFrame = Game():GetFrameCount() + duration })
+
+                        SFXManager():Play(soundId, volume)
+                        break
+                    end
                 end
             end
         end
