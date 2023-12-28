f9a8dd41ee072fe483e89aa014a4daa67cc23250
iwt2hw <iwt2hw@naver.com>
2023-12-28 22:45:02 +0900

    1.22
    1.22
    ●해당 아이템들이 신규 항목으로 업데이트 됩니다
    【패시브】
    Master's Degree(석사 학위)
    └모든 알약이 거대 알약으로 변경됩니다
    └알약의 효과를 알 수 있게 됩니다
    ●해당 아이템들의 배열 정보가 업데이트 됩니다
    【보스방】
    └신규 추가 배열 아이템 : #ANTIGRAVITY
    【상점방】
    └신규 추가 배열 아이템 : #TOOTH_AND_NAIL
    ●해당 아이템들의 동전 판매 가격이 업데이트 됩니다
    #TOOTH_AND_NAIL : 15 -> 5
    #PHD : 25 -> 20
    #ROSARY : 15 -> 11
    #EVIL_CHARM : 17 -> 10
    #LIL_CHEST : 10 -> 8
    #OPTIONS : 30 -> 23
    #MAGIC_8_BALL : 15 -> 9
    #VANISHING_TWIN : 15 -> 10
    #LUNA : 26 -> 20
    #LUCKY_FOOT : 25 -> 20
    Master's Degree : 15 -> 20
    Ward : 25 -> 20
    ●해당 아이템들의 하트 판매 가격이 업데이트 됩니다
    ●해당 캐릭터들의 정보가 업데이트 됩니다
    【에덴】
    └#LITTLE_STEVEN 시작아이템이 추가됩니다
    【유다】
    └Holy Card 등장하지 않습니다
    【로스트】
    └Holy Card 등장하지 않습니다
    ●해당 포켓 아이템들의 효과가 업데이트 됩니다
    ●해당 컬렉션 아이템들의 퀄리티가 업데이트 됩니다
    ●해당 컬렉션 아이템들의 효과가 업데이트 됩니다
    Key Is Power : 열쇠 1개당 공격력 상승치가 증가됩니다 (0.04 -> 0.16)
    Bomb Is Power : 폭탄 1개당 공격력 상승치가 증가됩니다 (0.04 -> 0.16)
    ●해당 장신구 아이템들의 효과가 업데이트 됩니다
    -#PERFECTION
    【9스테이지 보상】
    └기존 : 에덴의 축복 x 1
    └변경 : 다음 판 아이템을 소환하는 모든 축복류 아이템 중 랜덤 1개
    ●해당 아이템들의 쿨타임 수치가 업데이트 됩니다
    ●해당 몬스터들의 체력 및 방어막, 챔피언 유무가 업데이트 됩니다
    -더 이상 체력 감소 변종이 등장하지 않도록 변경됩니다
    ●해당 아이템들의 세트 취급 항목이 업데이트 됩니다
    ●기타 변경 사항 및 버그 수정
    -신규 시스템 : 에픽빔이 추가됩니다
    └획득 빈도가 낮은 아이템 등장 시 애니메이션과 사운드가 출력됩니다
    -상점방에서 구매 아이템들의 개수가 증가됩니다 (7 -> 8)
    -크리스마스 기념 스킨 제거됩니다
    -Key Is Power : 아이템 설명 오타가 수정됩니다

--- astro/epic.lua
+++ astro/epic.lua
@@ -3,13 +3,13 @@ local isc = require("astro.lib.isaacscript-common")
 local soundId = Isaac.GetSoundIdByName("Epic")
 
 -- 투명도 설정
-local alpha = 1
+local alpha = 1 -- (0.1 ~ 1) 까지 편하신대로 설정 하시면 됩니다 0일 경우 출력 안됨
 
 -- 볼륨 설정
-local volume = 0.7
+local volume = 0.7 -- (0.1 ~ 1) 까지 편하신대로 설정 하시면 됩니다 0일 경우 출력 안됨
 
 -- 지속 프레임
-local duration = 10 * 30 -- 10초
+local duration = 2.5 * 30 -- 오른쪽 30 은 건들지 마시고, 왼쪽 숫자만 건드시면 됩니다 2.5 * 30 -> 2.5초동안 에픽 애니메이션 출력
 
 local epics = {
     [RoomType.ROOM_TREASURE] = {
@@ -17,6 +17,23 @@ local epics = {
     },
     [RoomType.ROOM_SHOP] = {
         CollectibleType.COLLECTIBLE_TREASURE_MAP,
+    },
+    [RoomType.ROOM_BOSS] = {
+        CollectibleType.COLLECTIBLE_LIL_BRIMSTONE,
+        CollectibleType.COLLECTIBLE_SCORPIO,
+        CollectibleType.COLLECTIBLE_EDENS_BLESSING,
+        CollectibleType.COLLECTIBLE_SERAPHIM,
+        CollectibleType.COLLECTIBLE_SUCCUBUS,
+        CollectibleType.COLLECTIBLE_BROKEN_GLASS_CANNON,
+        CollectibleType.COLLECTIBLE_DOGMA,
+        CollectibleType.COLLECTIBLE_R_KEY,
+        CollectibleType.COLLECTIBLE_SAUSAGE,
+        CollectibleType.COLLECTIBLE_PASCHAL_CANDLE,
+        CollectibleType.COLLECTIBLE_VIRGO,
+        CollectibleType.COLLECTIBLE_ROTTEN_BABY,
+        Astro.Collectible.ANGRY_ONION,
+        Astro.Collectible.HAPPY_ONION,
+        Astro.Collectible.GALACTIC_MEDAL_OF_VALOR,
     },
 }
 
