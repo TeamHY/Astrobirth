d1690e65d8e679a90d861fc4dd0b8b402c41fbb2
Pdom <pdom0327@gmail.com>
2024-01-08 11:30:41 +0900

    feat: pink ward #88

---
+++ astro/collectibles/customs/pink-ward.lua
@@ -0,0 +1,53 @@
+Astro.Collectible.PINK_WARD = Isaac.GetItemIdByName("Pink Ward")
+
+if EID then
+    EID:addCollectible(Astro.Collectible.PINK_WARD, "레드방을 포함한 스테이지 중심 5x5의 방을 보여줍니다.", "핑크 와드")
+end
+
+local function DisplayPinkWardRoom()
+    local level = Game():GetLevel()
+
+    local roomIndexs = {
+        56, 57, 58, 59, 60,
+        69, 70, 71, 72, 73,
+        82, 83, 84, 85, 86,
+        95, 96, 97, 98, 99,
+        108, 109, 110, 111, 112
+    }
+
+    for _, value in ipairs(roomIndexs) do
+        local room = level:GetRoomByIdx(value)
+
+        if room.DisplayFlags & RoomDescriptor.DISPLAY_BOX ~= RoomDescriptor.DISPLAY_BOX then
+            room.DisplayFlags = room.DisplayFlags | RoomDescriptor.DISPLAY_BOX | RoomDescriptor.DISPLAY_ICON
+        end
+    end
+
+    level:UpdateVisibility()
+end
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_NEW_LEVEL,
+    function(_)
+        for i = 1, Game():GetNumPlayers() do
+            local player = Isaac.GetPlayer(i - 1)
+
+            if player:HasCollectible(Astro.Collectible.PINK_WARD) then
+                DisplayPinkWardRoom()
+                break
+            end
+        end
+    end
+)
+
+Astro:AddCallbackCustom(
+    isc.ModCallbackCustom.POST_PLAYER_COLLECTIBLE_ADDED,
+    ---@param player EntityPlayer
+    ---@param collectibleType CollectibleType
+    function(_, player, collectibleType)
+        if Astro:IsFirstAdded(Astro.Collectible.PINK_WARD) then
+            DisplayPinkWardRoom()
+        end
+    end,
+    Astro.Collectible.PINK_WARD
+)
