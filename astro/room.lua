fea5ff4d946dda70509c1ed2f172b0231b12df86
Pdom <pdom0327@gmail.com>
2024-01-20 17:23:05 +0900

    feat: beast room #109

--- astro/room.lua
+++ astro/room.lua
@@ -259,3 +259,25 @@ Astro:AddCallback(
         end
     end
 )
+
+Astro:AddCallback(
+    ModCallbacks.MC_ENTITY_TAKE_DMG,
+    ---@param entity Entity
+    ---@param amount number
+    ---@param damageFlags number
+    ---@param source EntityRef
+    ---@param countdownFrames number
+    function(_, entity, amount, damageFlags, source, countdownFrames)
+        local player = entity:ToPlayer()
+
+        if player ~= nil then
+                if damageFlags & (DamageFlag.DAMAGE_NO_PENALTIES | DamageFlag.DAMAGE_RED_HEARTS) == 0 then
+                    local currentRoomDesc = Game():GetLevel():GetCurrentRoomDesc()
+
+                    if currentRoomDesc.Data.Name == "Beast Room" then
+                        Astro.Data.DamoclesKill = true
+                    end
+                end
+        end
+    end
+)
