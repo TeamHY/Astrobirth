c00d28ee2dde2a3053ee64f82b4fa7c8403c4f16
Pdom <pdom0327@gmail.com>
2024-02-14 01:34:52 +0900

    feat: bachelor's degree #126
    알약의 효과를 보여주는 기능은 제거했습니다. 다시 활성화를 원하시면 주석을 확인해주세요.

--- astro/collectibles/customs/bachelors-degree.lua
+++ astro/collectibles/customs/bachelors-degree.lua
@@ -1,10 +1,10 @@
 Astro.Collectible.BACHELORS_DEGREE = Isaac.GetItemIdByName("Bachelor's Degree")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.BACHELORS_DEGREE, "{{Pill1}}Gulp!와 스탯 관련 알약만 등장합니다.#알약의 효과를 보여줍니다.", "학사학위")
+    EID:addCollectible(Astro.Collectible.BACHELORS_DEGREE, "{{Pill1}}Gulp!와 스탯 관련 알약만 등장합니다.", "학사학위")
 end
 
-local whitelist = {
+local normalWhitelist = {
     -- PillEffect.PILLEFFECT_HEALTH_DOWN,
     -- PillEffect.PILLEFFECT_HEALTH_UP,
     PillEffect.PILLEFFECT_RANGE_DOWN,
@@ -20,6 +20,35 @@ local whitelist = {
     PillEffect.PILLEFFECT_SHOT_SPEED_UP,
 }
 
+local positiveWhitelist = {
+    -- PillEffect.PILLEFFECT_HEALTH_UP,
+    PillEffect.PILLEFFECT_RANGE_UP,
+    PillEffect.PILLEFFECT_SPEED_UP,
+    PillEffect.PILLEFFECT_TEARS_UP,
+    PillEffect.PILLEFFECT_LUCK_UP,
+    PillEffect.PILLEFFECT_GULP,
+    PillEffect.PILLEFFECT_SHOT_SPEED_UP,
+}
+
+-- 알약 효과를 알수 있는 기능을 원하시면 아래 주석(--)을 제거해주세요.
+
+-- Astro:AddCallback(
+--     ModCallbacks.MC_POST_PICKUP_INIT,
+--     ---@param pickup EntityPickup
+--     function(_, pickup)
+--         if pickup.Variant == PickupVariant.PICKUP_PILL then
+--             for i = 1, Game():GetNumPlayers() do
+--                 local player = Isaac.GetPlayer(i - 1)
+
+--                 if player:HasCollectible(Astro.Collectible.BACHELORS_DEGREE) then
+--                     Game():GetItemPool():IdentifyPill(pickup.SubType)
+--                     break
+--                 end
+--             end
+--         end
+--     end
+-- )
+
 Astro:AddCallback(
     ModCallbacks.MC_POST_GAME_STARTED,
     function(_, isContinued)
@@ -31,7 +60,7 @@ Astro:AddCallback(
 
 ---@param pillEffect PillEffect
 ---@return boolean
-local function CheckWhitelist(pillEffect)
+local function CheckWhitelist(pillEffect, whitelist)
     for _, value in ipairs(whitelist) do
         if value == pillEffect then
             return true
@@ -58,34 +87,23 @@ Astro:AddCallback(
             local player = Isaac.GetPlayer(i - 1)
 
             if player:HasCollectible(Astro.Collectible.BACHELORS_DEGREE) then
-                if CheckWhitelist(selectedPillEffect) then
+                local whiteList = normalWhitelist
+
+                if player:HasCollectible(CollectibleType.COLLECTIBLE_PHD) or player:HasCollectible(CollectibleType.COLLECTIBLE_LUCKY_FOOT) or player:HasCollectible(CollectibleType.COLLECTIBLE_VIRGO) then
+                    whiteList = positiveWhitelist
+                end
+
+                if CheckWhitelist(selectedPillEffect, whiteList) then
                     Astro.Data.Pills[pillColor] = selectedPillEffect
                 else
                     local rng = player:GetCollectibleRNG(Astro.Collectible.BACHELORS_DEGREE)
-                    local index = rng:RandomInt(#whitelist) + 1
-                    local newPillEffect = whitelist[index]
+                    local index = rng:RandomInt(#whiteList) + 1
+                    local newPillEffect = whiteList[index]
     
                     Astro.Data.Pills[pillColor] = newPillEffect
                 end
 
                 return Astro.Data.Pills[pillColor]
-            end
-        end
-    end
-)
-
-Astro:AddCallback(
-    ModCallbacks.MC_POST_PICKUP_INIT,
-    ---@param pickup EntityPickup
-    function(_, pickup)
-        if pickup.Variant == PickupVariant.PICKUP_PILL then
-            for i = 1, Game():GetNumPlayers() do
-                local player = Isaac.GetPlayer(i - 1)
-
-                if player:HasCollectible(Astro.Collectible.BACHELORS_DEGREE) then
-                    Game():GetItemPool():IdentifyPill(pickup.SubType)
-                    break
-                end
             end
         end
     end
