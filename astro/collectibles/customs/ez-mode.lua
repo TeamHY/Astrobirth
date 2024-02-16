d401b887c84e79f8822185f29d86a270846944fa
Pdom <pdom0327@gmail.com>
2024-02-17 01:38:32 +0900

    feat: ez mode #127

--- astro/collectibles/customs/ez-mode.lua
+++ astro/collectibles/customs/ez-mode.lua
@@ -1,7 +1,7 @@
 Astro.Collectible.EZ_MODE = Isaac.GetItemIdByName("EZ Mode")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.EZ_MODE, "#스테이지를 넘어갈 때마다 {{Card51}}Holy Card를 하나 드랍합니다.", "이지 모드")
+    EID:addCollectible(Astro.Collectible.EZ_MODE, "스테이지를 넘어갈 때마다 {{Card51}}Holy Card를 하나 드랍합니다.#피격 페널티가 발생하지 않습니다.", "이지 모드")
 end
 
 Astro:AddCallback(
@@ -26,6 +26,27 @@ Astro:AddCallback(
         if Astro:CheckFirstVisitFrame(currentRoom) then
             if level:GetAbsoluteStage() == LevelStage.STAGE1_1 and level:GetCurrentRoomIndex() == 84 then
                 Astro:SpawnCollectible(Astro.Collectible.EZ_MODE, currentRoom:GetGridPosition(16), nil, true)
+            end
+        end
+    end
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_ENTITY_TAKE_DMG,
+    ---@param entity Entity
+    ---@param amount number
+    ---@param damageFlags number
+    ---@param source EntityRef
+    ---@param countdownFrames number
+    function(_, entity, amount, damageFlags, source, countdownFrames)
+        local player = entity:ToPlayer()
+
+        if player ~= nil then
+            if player:HasCollectible(Astro.Collectible.EZ_MODE) then
+                if damageFlags & DamageFlag.DAMAGE_NO_PENALTIES == 0 then
+                    player:TakeDamage(amount, damageFlags | DamageFlag.DAMAGE_NO_PENALTIES, source, countdownFrames)
+                    return false
+                end
             end
         end
     end
