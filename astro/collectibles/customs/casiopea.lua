a8e70b514b408a89261d8f84b3b251c2167967a7
Pdom <pdom0327@gmail.com>
2024-02-19 02:39:31 +0900

    feat: casiopea #128

--- astro/collectibles/customs/casiopea.lua
+++ astro/collectibles/customs/casiopea.lua
@@ -3,7 +3,7 @@ local isc = require("astro.lib.isaacscript-common")
 Astro.Collectible.CASIOPEA = Isaac.GetItemIdByName("Casiopea")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.CASIOPEA, "다음 게임 시작 시 랜덤 황금 장신구와 {{Pill1}}Gulp!가 소환됩니다.#중첩이 가능합니다.", "카시오페아")
+    EID:addCollectible(Astro.Collectible.CASIOPEA, "#획득 시 랜덤 황금 장신구와 {{Pill1}}Gulp!가 소환됩니다.#다음 게임 시작 시 랜덤 황금 장신구가 소환됩니다.#중첩이 가능합니다.", "카시오페아")
 end
 
 Astro:AddCallback(
@@ -12,28 +12,10 @@ Astro:AddCallback(
     function(_, isContinued)
         if not isContinued and Astro.Data.CasiopeaCount ~= nil and Astro.Data.CasiopeaCount > 0 then
             local player = Isaac.GetPlayer()
-            local game = Game()
-            local currentRoom = game:GetLevel():GetCurrentRoom()
-            local itemPool = game:GetItemPool()
-            local pillColor = itemPool:ForceAddPillEffect(PillEffect.PILLEFFECT_GULP)
+            local itemPool = Game():GetItemPool()
 
             for _ = 1, Astro.Data.CasiopeaCount do
-                Isaac.Spawn(
-                    EntityType.ENTITY_PICKUP,
-                    PickupVariant.PICKUP_PILL,
-                    pillColor,
-                    currentRoom:FindFreePickupSpawnPosition(player.Position, 40, true),
-                    Vector.Zero,
-                    nil
-                )
-                Isaac.Spawn(
-                    EntityType.ENTITY_PICKUP,
-                    PickupVariant.PICKUP_TRINKET,
-                    itemPool:GetTrinket() + 32768,
-                    currentRoom:FindFreePickupSpawnPosition(player.Position, 40, true),
-                    Vector.Zero,
-                    nil
-                )
+                Astro:SpawnTrinket(itemPool:GetTrinket() + 32768, player.Position)
             end
 
             Astro.Data.CasiopeaCount = 0
@@ -46,6 +28,14 @@ Astro:AddCallbackCustom(
     ---@param player EntityPlayer
     ---@param collectibleType CollectibleType
     function(_, player, collectibleType)
+        if Astro:IsFirstAdded(Astro.Collectible.CASIOPEA) then
+            local game = Game()
+            local itemPool = game:GetItemPool()
+
+            Astro:SpawnPill(PillEffect.PILLEFFECT_GULP, player.Position)
+            Astro:SpawnTrinket(itemPool:GetTrinket() + 32768, player.Position)
+        end
+
         Astro.Data.CasiopeaCount = player:GetCollectibleNum(Astro.Collectible.CASIOPEA)
     end,
     Astro.Collectible.CASIOPEA
