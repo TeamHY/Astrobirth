a54d1fa80695bb4236f6988ad30a3b4fc4c7774c
Pdom <pdom0327@gmail.com>
2024-02-27 23:27:08 +0900

    feat: epic #140

--- astro/epic.lua
+++ astro/epic.lua
@@ -1,21 +1,7 @@
-local soundId = Isaac.GetSoundIdByName("Epic")
-
--- 투명도 설정
-local alpha = 1 -- (0.1 ~ 1) 까지 편하신대로 설정 하시면 됩니다 0일 경우 출력 안됨
-
--- 볼륨 설정
-local volume = 0.6 -- (0.1 ~ 1) 까지 편하신대로 설정 하시면 됩니다 0일 경우 출력 안됨
-
--- 지속 프레임
-local duration = 2.2 * 30 -- 오른쪽 30 은 건들지 마시고, 왼쪽 숫자만 건드시면 됩니다 10 * 30 -> 10초동안 에픽 애니메이션 출력
-
--- 장면당 프레임 (숫자가 낮을수록 빠름)
-local speed = 1.1
-
 Astro:AddCallback(
     ModCallbacks.MC_POST_GAME_STARTED,
     function(_, isContinued)
-        if not isContinued or Astro.Data.Epice == nil then
+        if not isContinued or Astro.Data.Epics == nil then
             Astro.Data.Epics = {
                 common = {
                     CollectibleType.COLLECTIBLE_SACRED_HEART,
@@ -218,76 +204,6 @@ Astro:AddCallback(
                     Astro.Collectible.GALACTIC_MEDAL_OF_VALOR,
                 },
             }
-        end
-    end
-)
-
----@type {pickup: EntityPickup, sprite: Sprite, prevFrame: integer, endFrame: integer}[]
-local targets = {}
-
-Astro:AddCallback(
-    ModCallbacks.MC_POST_UPDATE,
-    function(_)
-        for _, target in ipairs(targets) do
-            local frameCount = Game():GetFrameCount()
-
-            if target.prevFrame + speed < frameCount then
-                target.sprite:Update()
-                target.prevFrame = frameCount
-            end
-        end
-    end
-)
-
-Astro:AddCallback(
-    ModCallbacks.MC_POST_RENDER,
-    function(_)
-        local removeIndexs = {}
-
-        for i, target in ipairs(targets) do
-            if target.pickup:Exists() and target.pickup.SubType ~= 0 and Game():GetFrameCount() < target.endFrame then
-                target.sprite:Render(Isaac.WorldToRenderPosition(target.pickup.Position + Vector(0, -40)), Vector(0, 0), Vector(0, 0))
-            else
-                table.insert(removeIndexs, i)
-            end
-        end
-
-        for _, value in ipairs(removeIndexs) do
-            table.remove(targets, value)
-        end
-    end
-)
-
-Astro:AddCallback(
-    ModCallbacks.MC_POST_PICKUP_INIT,
-    ---@param pickup EntityPickup
-    function(_, pickup)
-        if pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
-            local roomType = Game():GetLevel():GetCurrentRoom():GetType()
-
-            if Astro.Data.Epics then
-                local index = Astro:FindIndex(Astro.Data.Epics.common, pickup.SubType)
-                local targetList = Astro.Data.Epics.common
-
-                if index == -1 and Astro.Data.Epics[roomType] then
-                    index = Astro:FindIndex(Astro.Data.Epics[roomType], pickup.SubType)
-                    targetList = Astro.Data.Epics[roomType]
-                end
-
-                if index ~= -1 then
-                    local sprite = Sprite()
-                    sprite:Load("gfx/effects/epic.anm2", true)
-                    sprite.Color = Color(1, 1, 1, alpha, 0, 0, 0)
-                    sprite:Play("Play", true)
-
-                    local frameCount = Game():GetFrameCount()
-                    table.insert(targets, { pickup = pickup, sprite = sprite, prevFrame = frameCount, endFrame = frameCount + duration })
-
-                    SFXManager():Play(soundId, volume)
-
-                    table.remove(targetList, index)
-                end
-            end
         end
     end
 )
