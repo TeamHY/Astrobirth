c58a7d0f4c83f5eb784c1c0fe9ff2bbed3a3d2e3
Pdom <pdom0327@gmail.com>
2024-03-08 23:01:56 +0900

    feat: snake eye

--- astro/collectibles/customs/snake-eye.lua
+++ astro/collectibles/customs/snake-eye.lua
@@ -1,7 +1,7 @@
 Astro.Collectible.SNAKE_EYE = Isaac.GetItemIdByName("Sinful Spoils of Subversion - Snake Eye")
 
 if EID then
-    EID:addCollectible(Astro.Collectible.SNAKE_EYE, "눈물을 발사할 때마다 30%의 확률로 연옥의 유령을 소환합니다.#중첩 시 기본 확률이 합 연산으로 증가합니다.#!!! {{LuckSmall}}행운 수치 비례: 행운 70 이상일 때 100% 확률 (행운 1당 +1%p)", "반역의 죄보 - 스네이크아이")
+    EID:addCollectible(Astro.Collectible.SNAKE_EYE, "공격 시 30%의 확률로 연옥의 유령을 소환합니다.#중첩 시 기본 확률이 합 연산으로 증가합니다.#!!! {{LuckSmall}}행운 수치 비례: 행운 70 이상일 때 100% 확률 (행운 1당 +1%p)", "반역의 죄보 - 스네이크아이")
 end
 
 local spawnChance = 0.3
@@ -9,13 +9,17 @@ local spawnChance = 0.3
 local luckMultiply = 1 / 100
 
 Astro:AddCallback(
-    ModCallbacks.MC_POST_FIRE_TEAR,
-    ---@param tear EntityTear
-    function(_, tear)
-        local player = Astro:GetPlayerFromEntity(tear)
+    ModCallbacks.MC_ENTITY_TAKE_DMG,
+    ---@param entity Entity
+    ---@param amount number
+    ---@param damageFlags number
+    ---@param source EntityRef
+    ---@param countdownFrames number
+    function(_, entity, amount, damageFlags, source, countdownFrames)
+        local player = Astro:GetPlayerFromEntity(source.Entity)
 
-        if player ~= nil then
-            if player:HasCollectible(Astro.Collectible.SNAKE_EYE) then
+        if player ~= nil and player:HasCollectible(Astro.Collectible.SNAKE_EYE) and entity:IsVulnerableEnemy() then
+            if source.Type == EntityType.ENTITY_TEAR or damageFlags & DamageFlag.DAMAGE_LASER == DamageFlag.DAMAGE_LASER or source.Type == EntityType.ENTITY_KNIFE then
                 local rng = player:GetCollectibleRNG(Astro.Collectible.SNAKE_EYE)
                 local baseChance = spawnChance * player:GetCollectibleNum(Astro.Collectible.SNAKE_EYE);
 
