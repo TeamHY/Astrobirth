e528ee42bdfcf30736f0f6afaf373dcded3cb2e0
iwt2hw <iwt2hw@naver.com>
2024-03-09 23:53:20 +0900

    로튼 베이비 졷버그로 임시 제거
    로튼 베이비 졷버그로 임시 제거

--- astro/ban.lua
+++ astro/ban.lua
@@ -67,6 +67,7 @@ local banItem = {
             CollectibleType.COLLECTIBLE_GREEDS_GULLET,
             CollectibleType.COLLECTIBLE_BIRDS_EYE,
             CollectibleType.COLLECTIBLE_GODS_FLESH,
+            CollectibleType.COLLECTIBLE_ROTTEN_BABY,
         },
         trinket = {
             TrinketType.TRINKET_BROKEN_REMOTE,
