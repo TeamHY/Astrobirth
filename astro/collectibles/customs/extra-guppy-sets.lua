b858b83bbea5fda734f96219f53368adf1ad0de5
Pdom <pdom0327@gmail.com>
2024-03-13 16:59:16 +0900

    fix: rotten baby and extra guppy #153

--- astro/collectibles/customs/extra-guppy-sets.lua
+++ astro/collectibles/customs/extra-guppy-sets.lua
@@ -5,35 +5,52 @@ Astro:AddEIDCollectible(Astro.Collectible.ABSOLUT_GUPPY, "앱솔루트 구피", 
 Astro:AddEIDCollectible(Astro.Collectible.DELIRIUM_GUPPY, "섬망 구피", "...", "파란 아군 파리 대신 하얀 아군 파리가 소환됩니다.")
 
 local ABSOLUT_GUPPY_SUBTYPE = 1000
+local DELIRIUM_FLY_COLOR = Color(1, 1, 1, 1, 1, 1, 1)
+
+local isStarted = false
+
+Astro:AddCallback(
+    ModCallbacks.MC_POST_GAME_STARTED,
+    function(_, isContinued)
+        isStarted = true
+    end
+)
+
+Astro:AddCallback(
+    ModCallbacks.MC_PRE_GAME_EXIT,
+    ---@param shouldSave boolean
+    function(_, shouldSave)
+        isStarted = false
+    end
+)
 
 Astro:AddCallback(
     ModCallbacks.MC_FAMILIAR_INIT,
     ---@param familiar EntityFamiliar
     function(_, familiar)
         if familiar.Variant == FamiliarVariant.BLUE_FLY and familiar.SubType == 0 then
-            local subType = Astro:HasCollectible(Astro.Collectible.DELIRIUM_GUPPY) and 5 or ABSOLUT_GUPPY_SUBTYPE
+            local absolutFly;
 
-            if Astro:HasCollectible(Astro.Collectible.ABSOLUT_GUPPY) then
-                Isaac.Spawn(
+            if isStarted and Astro:HasCollectible(Astro.Collectible.ABSOLUT_GUPPY) then
+                absolutFly = Isaac.Spawn(
                     EntityType.ENTITY_FAMILIAR,
                     FamiliarVariant.BLUE_FLY,
-                    subType,
+                    ABSOLUT_GUPPY_SUBTYPE,
                     familiar.Position,
                     Vector.Zero,
                     familiar.SpawnerEntity
                 )
             end
 
-            familiar:Remove()
+            if Astro:HasCollectible(Astro.Collectible.DELIRIUM_GUPPY) then
+                familiar.Color = DELIRIUM_FLY_COLOR
 
-            Isaac.Spawn(
-                    EntityType.ENTITY_FAMILIAR,
-                    FamiliarVariant.BLUE_FLY,
-                    subType,
-                    familiar.Position,
-                    Vector.Zero,
-                    familiar.SpawnerEntity
-                )
+                if absolutFly then
+                    absolutFly.Color = DELIRIUM_FLY_COLOR
+                end
+            end
+        elseif familiar.Variant == FamiliarVariant.BLUE_FLY and familiar.SubType == ABSOLUT_GUPPY_SUBTYPE then
+            familiar.Color = DELIRIUM_FLY_COLOR
         end
     end
 )
