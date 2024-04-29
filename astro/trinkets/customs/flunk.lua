900eae4a5d469fffb77a363b393d397d04ba5fbc
Pdom <pdom0327@gmail.com>
2024-04-29 18:20:40 +0900

    feat: flunk #188

--- astro/trinkets/customs/flunk.lua
+++ astro/trinkets/customs/flunk.lua
@@ -9,3 +9,21 @@ if EID then
 
     -- Astro:AddGoldenTrinketDescription(Astro.Trinket.FLUNK, "", 10)
 end
+
+Astro:AddCallback(
+    ModCallbacks.MC_ENTITY_TAKE_DMG,
+    ---@param entity Entity
+    ---@param amount number
+    ---@param damageFlags number
+    ---@param source EntityRef
+    ---@param countdownFrames number
+    function(_, entity, amount, damageFlags, source, countdownFrames)
+        local player = entity:ToPlayer()
+
+        if player ~= nil and player:HasTrinket(Astro.Trinket.FLUNK) then
+            if damageFlags & (DamageFlag.DAMAGE_NO_PENALTIES | DamageFlag.DAMAGE_RED_HEARTS) == 0 then
+                Astro:RemoveAllTrinket(player, Astro.Trinket.FLUNK)
+            end
+        end
+    end
+)
