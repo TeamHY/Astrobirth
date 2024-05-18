74d9ed826d2b6a602ec4d33e22bba875d0311594
Pdom <pdom0327@gmail.com>
2024-05-18 15:28:02 +0900

    fix: aries ex #210

--- astro/collectibles/customs/aries-ex.lua
+++ astro/collectibles/customs/aries-ex.lua
@@ -1,14 +1,14 @@
 Astro.Collectible.ARIES_EX = Isaac.GetItemIdByName("Aries EX")
 
 if EID then
-    Astro:AddEIDCollectible(Astro.Collectible.ARIES_EX, "초 양자리", "...", "피격 시 무효화 처리되며, 5초간 무적이 됩니다. 해당 아이템 소지 시 모든 방어막 계열 아이템이 발동되지 않습니다.{{Timer}}쿨타임 60초")
+    Astro:AddEIDCollectible(Astro.Collectible.ARIES_EX, "초 양자리", "...", "피격 시 무효화 처리되며, 10초간 무적이 됩니다. 해당 아이템 소지 시 모든 방어막 계열 아이템이 발동되지 않습니다.{{Timer}}쿨타임 60초")
 end
 
 --- 쿨타임
 local cooldown = 60 * 30
 
---- 지속 시간
-local duration = 5 * 30
+-- --- 지속 시간
+-- local duration = 5 * 30
 
 Astro:AddCallback(
     ModCallbacks.MC_ENTITY_TAKE_DMG,
@@ -25,8 +25,7 @@ Astro:AddCallback(
 
             if data.Aries == nil then
                 data.Aries = {
-                    CooldownTime = 0,
-                    DurationTime = 0
+                    CooldownTime = 0
                 }
             end
 
@@ -34,29 +33,12 @@ Astro:AddCallback(
 
             if data.Aries.CooldownTime <= frameCount then
                 data.Aries.CooldownTime = frameCount + cooldown
-                data.Aries.DurationTime = frameCount + duration
 
-                player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS, true)
+                player:UseActiveItem(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS, false)
 
                 return false
             end
         end
     end,
     EntityType.ENTITY_PLAYER
-)
-
-Astro:AddCallback(
-    ModCallbacks.MC_POST_UPDATE,
-    function()
-        for i = 1, Game():GetNumPlayers() do
-            local player = Isaac.GetPlayer(i - 1)
-            local data = player:GetData()
-
-            if data.Aries ~= nil then
-                if data.Aries.DurationTime <= Game():GetFrameCount() then
-                    player:GetEffects():RemoveCollectibleEffect(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS)
-                end
-            end
-        end
-    end
 )
