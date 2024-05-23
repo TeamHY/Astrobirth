82c9432d2ee6f8885a99887108a2dbbc19e5f7a9
iwt2hw <iwt2hw@naver.com>
2024-05-23 17:59:02 +0900

    eid: Libra EX

--- astro/collectibles/customs/libra-ex.lua
+++ astro/collectibles/customs/libra-ex.lua
@@ -5,7 +5,7 @@ Astro.Collectible.LIBRA_EX = Isaac.GetItemIdByName("Libra EX")
 if EID then
     EID:assignTransformation("collectible", Astro.Collectible.LIBRA_EX, "Chubby")
     
-    Astro:AddEIDCollectible(Astro.Collectible.LIBRA_EX, "초 천칭자리", "...", "모든 세트 파츠들의 소지 카운트를 +1 상승되며, 동전, 폭탄, 열쇠 +7 증가됩니다.#처비 세트를 제외하고 중복이 가능합니다.")
+    Astro:AddEIDCollectible(Astro.Collectible.LIBRA_EX, "초 천칭자리", "...", "모든 세트 파츠들의 소지 카운트를 +1 상승됩니다.#처비 세트를 제외하고 중복이 가능합니다.")
 end
 
 Astro:AddCallbackCustom(
