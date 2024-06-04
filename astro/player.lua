2b1f48df4736b42b9cd37cc0e81950ed5a333ce0
iwt2hw <iwt2hw@naver.com>
2024-06-04 21:26:39 +0900

    player: 막달린 시작 소환 아이템

--- astro/player.lua
+++ astro/player.lua
@@ -32,7 +32,7 @@ local startItem = {
     },
     [PlayerType.PLAYER_MAGDALENE] = {
         collectible = {
-            -- CollectibleType.COLLECTIBLE_SAD_ONION,
+            AstroItems.Collectible.THE_HOLY_BLOOD_AND_THE_HOLY_GRAIL,
         },
         trinket = {
             -- TrinketType.TRINKET_SWALLOWED_PENNY,
