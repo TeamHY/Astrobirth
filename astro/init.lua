11a946c06dbd66438a0af1d619722fff982b6593
Pdom <pdom0327@gmail.com>
2024-08-22 00:48:45 +0900

    fix: qp overwrite

--- astro/init.lua
+++ astro/init.lua
@@ -1,30 +1,51 @@
-function Astro:CurseRemove(curse) -- 입장 전 저주 제거
-	local hasPrometheus = false
-	local hasCurseCleaner = false
+QP_OVERWRITE = {
+	isInfinity = false,
+	range = 80,
+	isDisable = false
+}
 
-	for i = 1, Game():GetNumPlayers() do
-		local player = Isaac.GetPlayer(i - 1)
+Astro:AddCallback(
+	ModCallbacks.MC_POST_GAME_STARTED,
+	function(_, isContinued)
+		if not isContinued then
+			QP_OVERWRITE = {
+				isInfinity = false,
+				range = 80,
+				isDisable = false
+			}
+		end
+	end
+)
 
-		if player:HasCollectible(AstroItems.Collectible.PROMETHEUS) then
-			hasPrometheus = true
+-- 저주 처리
+Astro:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL,
+	function (_, curse)
+		local hasPrometheus = false
+		local hasCurseCleaner = false
+
+		for i = 1, Game():GetNumPlayers() do
+			local player = Isaac.GetPlayer(i - 1)
+
+			if player:HasCollectible(AstroItems.Collectible.PROMETHEUS) then
+				hasPrometheus = true
+			end
+
+			if Astro:HasPerfectionEffect(player) or player:HasTrinket(AstroItems.Trinket.DOCTRINE) then
+				hasCurseCleaner = true
+			end
 		end
 
-		if Astro:HasPerfectionEffect(player) or player:HasTrinket(AstroItems.Trinket.DOCTRINE) then
-			hasCurseCleaner = true
+		if hasPrometheus and not hasCurseCleaner then
+			return curse | LevelCurse.CURSE_OF_DARKNESS
+		end
+
+		if (Game():GetLevel():GetStage() <= 3 and curse ~= 0) or hasCurseCleaner then
+			return 0
 		end
 	end
+)
 
-	if hasPrometheus and not hasCurseCleaner then
-		return curse | LevelCurse.CURSE_OF_DARKNESS
-	end
-
-	if (Game():GetLevel():GetStage() <= 3 and curse ~= 0) or hasCurseCleaner then
-		return 0
-	end
-end
-
-Astro:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, Astro.CurseRemove)
-
+-- 데드갓 체크
 Astro:AddCallback(
 	ModCallbacks.MC_POST_RENDER,
 	function(_)
@@ -38,19 +59,6 @@ Astro:AddCallback(
 	
 				Isaac.RenderText("Death Certificate is not unlocked", position.X - 100, position.Y, 255, 0, 0, 255)
 			end
-		end
-	end
-)
-
-Astro:AddCallback(
-	ModCallbacks.MC_POST_GAME_STARTED,
-	function(_, isContinued)
-		if not QP_OVERWRITE or not isContinued then
-			QP_OVERWRITE = {
-				isInfinity = false,
-				range = 80,
-				isDisable = false
-			}
 		end
 	end
 )
