b536f45f11d7e781e6df828bf07010686d36aef8
iwt2hw <iwt2hw@naver.com>
2025-01-22 21:17:37 +0900

    파란 고추
    등장 확률 증가
    패널티 완화(0.5 -> 0.9)

--- astro-fight/collectibles/status.lua
+++ astro-fight/collectibles/status.lua
@@ -14,9 +14,9 @@ if EID then
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_LOST_CONTACT then
                 EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.75")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_GHOST_PEPPER then
-                EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.5#↓ {{LuckSmall}}행운 -5")
+                EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.9#↓ {{LuckSmall}}행운 -5")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_BIRDS_EYE then
-                EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.5#↓ {{LuckSmall}}행운 -5")
+                EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.9#↓ {{LuckSmall}}행운 -5")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_C_SECTION then
                 EID:appendToDescription(descObj, "#↓ {{DamageSmall}}공격력 배율 x0.95")
             elseif descObj.ObjSubType == CollectibleType.COLLECTIBLE_IT_HURTS then
@@ -67,11 +67,11 @@ Astro:AddCallback(
             end
 
             if player:HasCollectible(CollectibleType.COLLECTIBLE_GHOST_PEPPER) then
-                player.Damage = player.Damage * 0.5
+                player.Damage = player.Damage * 0.9
             end
 
             if player:HasCollectible(CollectibleType.COLLECTIBLE_BIRDS_EYE) then
-                player.Damage = player.Damage * 0.5
+                player.Damage = player.Damage * 0.9
             end
 
             if player:HasCollectible(CollectibleType.COLLECTIBLE_C_SECTION) then
