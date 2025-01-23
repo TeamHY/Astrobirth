9c558d181b8858207c33f31bbaf85510414adda4
Pdom <pdom0327@gmail.com>
2025-01-23 19:42:16 +0900

    feat: 스피드 보정 #504

---
+++ astro-fight/collectibles/vanillas/the-small-rock.lua
@@ -0,0 +1,35 @@
+if EID then
+    EID:addDescriptionModifier(
+        "AstroCollectiblesTheSmallRock",
+        function(descObj)
+            if descObj.ObjType == EntityType.ENTITY_PICKUP and descObj.ObjVariant == PickupVariant.PICKUP_COLLECTIBLE and descObj.ObjSubType == CollectibleType.COLLECTIBLE_SMALL_ROCK then
+                return true
+            end
+        end,
+        function(descObj)
+            EID:appendToDescription(descObj, "#↑ {{SpeedSmall}}이동속도 +0.1 or +0.2")
+
+            return descObj
+        end
+    )
+end
+
+Astro:AddCallback(
+    ModCallbacks.MC_EVALUATE_CACHE,
+    ---@param player EntityPlayer
+    ---@param cacheFlag CacheFlag
+    function(_, player, cacheFlag)
+        local rng = Astro:CopyRNG(player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_SMALL_ROCK))
+
+        for _ = 1, player:GetCollectibleNum(CollectibleType.COLLECTIBLE_SMALL_ROCK) do
+            local random = rng:RandomInt(2)
+
+            if random == 0 then
+                player.MoveSpeed = player.MoveSpeed + 0.1
+            elseif random == 1 then
+                player.MoveSpeed = player.MoveSpeed + 0.2
+            end
+        end
+    end,
+    CacheFlag.CACHE_SPEED
+)
