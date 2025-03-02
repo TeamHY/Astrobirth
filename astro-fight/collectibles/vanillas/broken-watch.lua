671d465a4c46ebeb4f0b494f73c8d0eec6c0abda
Pdom <pdom0327@gmail.com>
2025-03-03 02:48:58 +0900

    feat(broken-watch): 이속 증가

---
+++ astro-fight/collectibles/vanillas/broken-watch.lua
@@ -0,0 +1,30 @@
+if EID then
+    EID:addDescriptionModifier(
+        "AstroCollectiblesBrokenWatch",
+        function(descObj)
+            if
+                descObj.ObjType == EntityType.ENTITY_PICKUP and descObj.ObjVariant == PickupVariant.PICKUP_COLLECTIBLE and
+                    descObj.ObjSubType == CollectibleType.COLLECTIBLE_BROKEN_WATCH
+             then
+                return true
+            end
+        end,
+        function(descObj)
+            EID:appendToDescription(descObj, "#↑ {{SpeedSmall}}이동속도 +0.3")
+
+            return descObj
+        end
+    )
+end
+
+Astro:AddCallback(
+    ModCallbacks.MC_EVALUATE_CACHE,
+    ---@param player EntityPlayer
+    ---@param cacheFlag CacheFlag
+    function(_, player, cacheFlag)
+        if cacheFlag == CacheFlag.CACHE_SPEED then
+            player.MoveSpeed = player.MoveSpeed + 0.3
+        end
+    end,
+    CacheFlag.CACHE_SPEED
+)
