36a19922c28837f63b1e3da044ef2b6b9380bc65
sawalk <ham.step2@gmail.com>
2026-01-15 18:32:11 +0900

    feat(eternal-d6): 스테이지 타입별로 데이터 저장

--- astro-fight/collectibles/vanillas/eternal-d6.lua
+++ astro-fight/collectibles/vanillas/eternal-d6.lua
@@ -47,14 +47,17 @@ Astro:AddCallback(
     ---@param activeSlot ActiveSlot
     ---@param varData integer
     function(_, collectibleType, rng, playerWhoUsedItem, useFlags, activeSlot, varData)
+        local level = game:GetLevel()
+        local stage, stageType = level:GetStage(), level:GetStageType()
+        local stageCheck = tostring(stage) .. "." .. tostring(stageType)
+
         local data = Astro:GetPersistentPlayerData(playerWhoUsedItem)
-        local stage = game:GetLevel():GetStage()
-        data.eternalD6Available[tostring(stage)] = data.eternalD6Available[tostring(stage)] or 1
+        data.eternalD6Available[stageCheck] = data.eternalD6Available[tostring(stageCheck)] or 1
 
         if collectibleType == CollectibleType.COLLECTIBLE_ETERNAL_D6 then
             if playerWhoUsedItem:GetPlayerType() == PlayerType.PLAYER_THELOST then
-                if data.eternalD6Available[tostring(stage)] == 1 then
-                    data.eternalD6Available[tostring(stage)] = 0
+                if data.eternalD6Available[tostring(stageCheck)] == 1 then
+                    data.eternalD6Available[tostring(stageCheck)] = 0
                 end
             end
         elseif collectibleType == (CollectibleType.COLLECTIBLE_FORGET_ME_NOW or CollectibleType.COLLECTIBLE_R_KEY) then
@@ -73,11 +76,14 @@ Astro:AddCallback(
     ---@param varData integer
     function(_, collectibleType, rng, playerWhoUsedItem, useFlags, activeSlot, varData)
         if playerWhoUsedItem:GetPlayerType() == PlayerType.PLAYER_THELOST then
-            local data = Astro:GetPersistentPlayerData(playerWhoUsedItem)
-            local stage = game:GetLevel():GetStage()
-            data.eternalD6Available[tostring(stage)] = data.eternalD6Available[tostring(stage)] or 1
+            local level = game:GetLevel()
+            local stage, stageType = level:GetStage(), level:GetStageType()
+            local stageCheck = tostring(stage) .. "." .. tostring(stageType)
 
-            if data.eternalD6Available[tostring(stage)] == 0 then
+            local data = Astro:GetPersistentPlayerData(playerWhoUsedItem)
+            data.eternalD6Available[tostring(stageCheck)] = data.eternalD6Available[tostring(stageCheck)] or 1
+
+            if data.eternalD6Available[tostring(stageCheck)] == 0 then
                 local saveCharge = playerWhoUsedItem:GetActiveCharge(activeSlot)
                 Astro:ScheduleForUpdate(
                     function()
