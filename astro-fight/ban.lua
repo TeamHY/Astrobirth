dfceef9fc01121abd32d921c1adf927df3a3067d
iwt2hw <iwt2hw@naver.com>
2026-05-23 12:01:28 +0900

    ban：로스트, 알트로스트
    로스트, 알트로스트
    └신규 금지 아이템 : 뒤집기

--- astro-fight/ban.lua
+++ astro-fight/ban.lua
@@ -295,6 +295,7 @@ local banItems = {
     [PlayerType.PLAYER_THELOST] = {
         collectible = {
             CollectibleType.COLLECTIBLE_BOOK_OF_SECRETS,
+            CollectibleType.COLLECTIBLE_FLIP,
             CollectibleType.COLLECTIBLE_LEO,
             CollectibleType.COLLECTIBLE_ANKH,
             CollectibleType.COLLECTIBLE_LAZARUS_RAGS,
@@ -627,6 +628,7 @@ local banItems = {
     [PlayerType.PLAYER_THELOST_B] = {
         collectible = {
             CollectibleType.COLLECTIBLE_BOOK_OF_SECRETS,
+            CollectibleType.COLLECTIBLE_FLIP,
             CollectibleType.COLLECTIBLE_LEO,
             CollectibleType.COLLECTIBLE_ANKH,
             CollectibleType.COLLECTIBLE_LAZARUS_RAGS,
