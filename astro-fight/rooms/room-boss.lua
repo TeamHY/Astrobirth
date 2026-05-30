6ce59f27c23e3f5b4486061b8018b4df3d676c62
Pdom <pdom0327@gmail.com>
2026-05-31 00:28:56 +0900

    feat: 자궁 보스 등장 조정 #954

--- astro-fight/rooms/room-boss.lua
+++ astro-fight/rooms/room-boss.lua
@@ -14,17 +14,34 @@ local STAGE_8_BOSS = {
     {908, 1, 0},
 }
 
+-- 적힌 번호를 포함해서 적용됩니다
+local MIN_ROOM_VARIANT_BY_STAGE_TYPE = {
+    [StageType.STAGETYPE_ORIGINAL] = 974, -- Womb
+    [StageType.STAGETYPE_WOTL] = 978, -- Utero
+    [StageType.STAGETYPE_AFTERBIRTH] = 987, -- Scarred Womb
+    [StageType.STAGETYPE_REPENTANCE] = 987, -- Corpse
+}
+
 ---
 
 local function IsRunStage()
     local level = Game():GetLevel()
     local stage = level:GetAbsoluteStage()
+    local stageType = level:GetStageType()
 
-    if (stage == LevelStage.STAGE4_1 or stage == LevelStage.STAGE4_2) and level:GetStageType() <= StageType.STAGETYPE_AFTERBIRTH then
+    if (stage == LevelStage.STAGE4_1 or stage == LevelStage.STAGE4_2) and MIN_ROOM_VARIANT_BY_STAGE_TYPE[stageType] ~= nil then
         return true
     end
 
     return false
+end
+
+local function IsRunRoomVariant()
+    local level = Game():GetLevel()
+    local minRoomVariant = MIN_ROOM_VARIANT_BY_STAGE_TYPE[level:GetStageType()]
+    local currentRoomDesc = level:GetCurrentRoomDesc()
+
+    return currentRoomDesc.Data.Variant >= minRoomVariant
 end
 
 Astro:AddCallback(
@@ -34,7 +51,7 @@ Astro:AddCallback(
         local stage = level:GetAbsoluteStage()
         local currentRoom = level:GetCurrentRoom()
 
-        if IsRunStage() and currentRoom:GetType() == RoomType.ROOM_DEFAULT and not currentRoom:IsClear() then
+        if IsRunStage() and IsRunRoomVariant() and currentRoom:GetType() == RoomType.ROOM_DEFAULT and not currentRoom:IsClear() then
             local position = currentRoom:GetCenterPos()
             local rng = Isaac.GetPlayer():GetCollectibleRNG(Astro.Collectible.CYGNUS)
 
