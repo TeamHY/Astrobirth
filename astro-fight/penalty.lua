9fd363ef031c64ee88ef96ca503646cc40b0fb00
iwt2hw <iwt2hw@naver.com>
2026-08-29 12:17:28 +0900

    feat：로스트, 알트로스트 패널티 피격 강화
    피격 시 행운 감소 3 -> 6

--- astro-fight/penalty.lua
+++ astro-fight/penalty.lua
@@ -6,7 +6,7 @@ local SPEED_DECREMENT = -0.05
 local TEARS_DECREMENT = -0
 local DAMAGE_DECREMENT = -0.05
 local RANGE_DECREMENT = -0.1
-local LUCK_DECREMENT = -3
+local LUCK_DECREMENT = -6
 
 local MAJOR_BOSS_ID = {
     [6]  =  true,   -- 엄마
